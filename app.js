079fb062bbcc5dac9157b675edb0eebeb6b8c42b
magento2Ext <36661813+magento2Ext@users.noreply.github.com>
2022-07-01 12:23:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req,res) => {
   res.send("welcome to the home page")
 });
 
-const port = process.env.port || 3000
+const port = process.env.port || 3001
 app.listen(port, () => {
     console.log('Server started')
 })
